f9e63215da015ddd3136793ae77464f50ca1c250
hatamna <118631041+hatamna@users.noreply.github.com>
2026-01-17 18:44:16 -0500

    Update MainDoc--.py

--- MainDoc--.py
+++ MainDoc--.py
@@ -1,6 +1,8 @@
-from tkinter import *
+import tkinter
+from tkinter import ttk
+import sv_ttk
 
-app = Tk()
+app = tkinter.Tk()
 
 # Code what the 'B' button does
 def bolding():
@@ -21,25 +23,26 @@ def compPara():
 app.title("Doc-- Simple AI-Powered Document Editor")
 app.geometry('700x450')
 
-button_frame = Frame(app)
+button_frame = ttk.Frame(app)
 button_frame.pack()
 
-bold_button = Button(button_frame, text="B", padx=5)
-bold_button.grid(row=0, column=0)
+bold_button = ttk.Button(button_frame, text="B")
+bold_button.grid(row=0, column = 0, padx=5)
 
-italic_button = Button(button_frame, text=' I ')
-italic_button.grid(row=0, column=1, padx=5)
+italic_button = ttk.Button(button_frame, text=' I ')
+italic_button.grid(row=0, column = 1)
 
-my_text = Text(app, width=85, height=20)
+my_text = tkinter.Text(app, width=85, height=20)
 my_text.pack(pady=20)
 
-button_frame2 = Frame(app)
+button_frame2 = ttk.Frame(app)
 button_frame2.pack()
 
-cs_button = Button(button_frame2, text="Complete Sentence")
-cs_button.grid(row=0, column=0, padx=5)
+cs_button = ttk.Button(button_frame2, text="Complete Sentence")
+cs_button.grid(row=0, column = 0)
 
-cpara_button = Button(button_frame2, text="Complete Paragraph")
-cpara_button.grid(row=0, column=1, padx=5)
+cpara_button = ttk.Button(button_frame2, text="Complete Paragraph")
+cpara_button.grid(row=0, column = 1, padx=5)
 
+sv_ttk.set_theme("dark")
 app.mainloop()
