be6befaa02e4522bb48444f4a4238e4986629c0c
Rosesh <roseshbaniya@gmail.com>
2026-01-17 18:52:13 -0500

    functional bold and italics button

--- MainDoc--.py
+++ MainDoc--.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import font
 from tkinter import ttk
 import sv_ttk
 
@@ -6,11 +7,32 @@ app = tkinter.Tk()
 
 # Code what the 'B' button does
 def bolding():
-    pass
+    #create font
+    boldfont=font.Font(my_text, my_text.cget("font"))
+    boldfont.configure(weight="bold")
+    #configure a tag
+    my_text.tag_configure("bold", font=boldfont)
+    #if statement to see if tag has been set
+    current_tags = my_text.tag_names("sel.first")
+
+    if "bold" in current_tags:
+        my_text.tag_remove("bold", "sel.first", "sel.last")
+    else:
+        my_text.tag_add("bold", "sel.first", "sel.last")
 
 # Code what the 'I' button does
 def italicising():
-    pass
+    #create font
+    italfont=font.Font(my_text, my_text.cget("font"))
+    italfont.configure(slant="italic")
+    #configure a tag
+    my_text.tag_configure("italic", font=italfont)
+    #if statement to see if tag has been set
+    current_tags = my_text.tag_names("sel.first")
+    if "italic" in current_tags:
+        my_text.tag_remove("italic", "sel.first", "sel.last")
+    else:
+        my_text.tag_add("italic", "sel.first", "sel.last")
 
 # Code what the 'Complete Sentence' button does (using Gemini)
 def compSent():
@@ -26,10 +48,10 @@ app.geometry('700x450')
 button_frame = ttk.Frame(app)
 button_frame.pack()
 
-bold_button = ttk.Button(button_frame, text="B")
+bold_button = ttk.Button(button_frame, text="B", command=bolding)
 bold_button.grid(row=0, column = 0, padx=5)
 
-italic_button = ttk.Button(button_frame, text=' I ')
+italic_button = ttk.Button(button_frame, text=' I ', command=italicising)
 italic_button.grid(row=0, column = 1)
 
 my_text = tkinter.Text(app, width=85, height=20)
