3b9ddf7e65d27817cb54b94a8ae5704eed3fd1f8
Rosesh <roseshbaniya@gmail.com>
2026-01-17 19:18:11 -0500

    bolditalics

--- MainDoc--.py
+++ MainDoc--.py
@@ -5,33 +5,42 @@ import sv_ttk
 
 app = tkinter.Tk()
 
+def apply_style_tags(start, end):
+    # Check what styles are currently ON at the start of selection
+    tags = set(my_text.tag_names(start))
+    bold_on = "bold" in tags
+    italic_on = "italic" in tags
+
+    # Always remove the combo tag first, then re-add if needed
+    my_text.tag_remove("bold_italic", start, end)
+
+    if bold_on and italic_on:
+        my_text.tag_add("bold_italic", start, end)
+
 # Code what the 'B' button does
 def bolding():
-    #create font
-    boldfont=font.Font(my_text, my_text.cget("font"))
-    boldfont.configure(weight="bold")
-    #configure a tag
-    my_text.tag_configure("bold", font=boldfont)
-    #if statement to see if tag has been set
-    current_tags = my_text.tag_names("sel.first")
-    if "bold" in current_tags:
-        my_text.tag_remove("bold", "sel.first", "sel.last")
+    if not my_text.tag_ranges("sel"):
+        return
+    start, end = "sel.first", "sel.last"
+    if "bold" in my_text.tag_names(start):
+        my_text.tag_remove("bold", start, end)
     else:
-        my_text.tag_add("bold", "sel.first", "sel.last")
+        my_text.tag_add("bold", start, end)
+    apply_style_tags(start, end)
 
 # Code what the 'I' button does
 def italicising():
-    #create font
-    italfont=font.Font(my_text, my_text.cget("font"))
-    italfont.configure(slant="italic")
-    #configure a tag
-    my_text.tag_configure("italic", font=italfont)
-    #if statement to see if tag has been set
-    current_tags = my_text.tag_names("sel.first")
-    if "italic" in current_tags:
-        my_text.tag_remove("italic", "sel.first", "sel.last")
+    if not my_text.tag_ranges("sel"):
+        return
+    if not my_text.tag_ranges("sel"):
+        return
+    start, end = "sel.first", "sel.last"
+    if "italic" in my_text.tag_names(start):
+        my_text.tag_remove("italic", start, end)
     else:
-        my_text.tag_add("italic", "sel.first", "sel.last")
+        my_text.tag_add("italic", start, end)
+
+    apply_style_tags(start, end)
 
 # Code what the 'Complete Sentence' button does (using Gemini)
 def compSent():
@@ -55,8 +64,19 @@ italic_button.grid(row=0, column = 1)
 
 base_font = font.Font(family="Courier New", size=12)
 
-my_text = tkinter.Text(app, width=85, height=20, font=base_font)
+my_text = tkinter.Text(app, width=85, height=20, font=base_font, exportselection=0)
 my_text.pack(pady=20)
+
+# Define text tags for styling
+bold_font = font.Font(my_text, my_text.cget("font"))
+bold_font.configure(weight="bold")
+italic_font = font.Font(my_text, my_text.cget("font"))
+italic_font.configure(slant="italic")
+bold_italic_font = font.Font(my_text, my_text.cget("font"))
+bold_italic_font.configure(weight="bold", slant="italic")
+my_text.tag_configure("bold", font=bold_font)
+my_text.tag_configure("italic", font=italic_font)
+my_text.tag_configure("bold_italic", font=bold_italic_font)
 
 button_frame2 = ttk.Frame(app)
 button_frame2.pack()
